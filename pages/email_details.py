8a03d3879940b6d8e99e268810c85eda9975460c
Taona Munikwa <69734394+taonaben@users.noreply.github.com>
2024-07-01 22:55:49 +0200

    cleaned up the code

--- pages/email_details.py
+++ pages/email_details.py
@@ -1,3 +1,14 @@
 password = 'lntk qjxy wwfk armx'
 email_address = 'munikwataona09@gmail.com'
-email_address1 = 'taonbenjamin180903@gmail.com'
+# email_address1 = 'taonbenjamin180903@gmail.com'
+email_subject = "IT Internship Inquiry"
+email_body = f"Dear Sir/ Madam,\n\n" \
+             f"I am writing to express my interest in the IT Internship position currently available" \
+             f" at your esteemed organization. I have attached my comprehensive CV and a detailed cover" \
+             f" letter, which highlights my relevant skills, experience, amd qualifications for the role." \
+             f"\n\nI would like to thank you for considering my application, and I look forward to the " \
+             f"opportunity to discuss my candidacy further. Please feel free to contact me at your convenience" \
+             f" to schedule an interview or request additional information.\n\nThank you for your time and" \
+             f" consideration.\n\nSincerely,\nTaona Munikwa B "
+
+attachment = r'C:\Users\taonb\PycharmProjects\Application letter Generator\letters\Taona Munikwa Curriculum Vitae.pdf'
--- pages/full_system.py
+++ pages/full_system.py
@@ -3,7 +3,7 @@ import pandas as pd
 import smtplib
 import ssl
 from email.message import EmailMessage
-from email_details import password, email_adress
+from email_details import password, email_address, email_body, email_subject, attachment
 import os
 
 def fill_letter(letter_path, output_path, data):
@@ -52,17 +52,20 @@ def generate_letter_and_send_emails(csv_path, letter_path, email_sender, email_p
         output_path = f"Cover letter for {company_name}.docx"
         fill_letter(letter_path, output_path, data)
 
-        subject = 'IT Internship Inquiry'
-        body = f"Dear Sir/ Madam,\n\nI am writing to express my interest in the IT Internship position currently available at your esteemed organization. I have attached my comprehensive CV and a detailed cover letter, which highlights my relevant skills, experience, amd qualifications for the role.\n\nI would like to thank you for considering my application, and I look forward to the opportunity to discuss my candidacy further. Please feel free to contact me at your convenience to schedule an interview or request additional information.\n\nThank you for your time and consideration.\n\nSincerely,\nTaona Munikwa B "
-        attachment_paths = [output_path, r'C:\Users\taonb\PycharmProjects\Application letter Generator\letters\Taona Munikwa Curriculum Vitae.pdf']
+        subject = email_subject
+        body = email_body
+        attachment_paths = [output_path, attachment]
 
         send_email_with_attachment(email_sender, email_password, email_receiver, subject, body, attachment_paths)
 
+        print(f'Email for {company_name} sent')
+        print('------------------------------')
+
 if __name__ == '__main__':
-    csv_path = r'C:\Users\taonb\PycharmProjects\Application letter Generator\venv\mock companies.csv'
+    csv_path = r'C:\Users\taonb\PycharmProjects\Application letter Generator\venv\companies.csv'
     letter_path = '../letters/Cover Letter.docx'
-    email_sender = email_adress
+    email_sender = email_address
     email_password = password
 
     generate_letter_and_send_emails(csv_path, letter_path, email_sender, email_password)
-    print(f'Letters sent')
+    print(f'\n\nLetters sent')
